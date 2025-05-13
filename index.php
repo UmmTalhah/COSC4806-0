2d0f761f124de58521a819fb0f5da2a76e6d65c0
Ayan J <90760565+UmmTalhah@users.noreply.github.com>
2025-05-13 01:45:46 +0000

    Added h1 tag

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+    <?php echo '<p>Hello, Class</p>'; ?> 
+    <h1> Welcome to my website </h1>
 
 </html>
